cd16bd40b639a10a125c0957948cca96ce54451f
nthhanh <nthhanh111994@gmail.com>
2019-07-29 16:50:21 +0700

    N4: MVC + Slipt files based on MVC pattent. + Add function read/write file using fs module. + In Model: using path moudle to setting path when read/write. + In Controller: Call method of Controller and render in View. + In View: template using template engine PUG.

--- routes/product.js
+++ routes/product.js
@@ -1,23 +1,10 @@
 const express = require('express');
-const path = require('path');
-const mainRouter = require('./main');
+const productComponent = require('../components/product');
 
 const router = express.Router();
-router.use('/product-list', (req, res, next) => {
-    res.render('shop/product-list', { prods: mainRouter.Products });
-});
 
-router.post('/add-product', (req, res, next) => {
-    const productName = req.body.productName;
-    console.log('1111111', productName);
-    if (productName) {
-        mainRouter.Products.push({ title: productName});
-    }
-    res.redirect('/product-list');
-});
+router.get('/add-product', productComponent.getAddProduct);
 
-router.get('/product', (req, res, next) => {
-    res.redirect('/');
-});
+router.post('/add-product', productComponent.postAddProduct);
 
 module.exports = router;
